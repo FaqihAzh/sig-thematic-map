3151f7d7b013497d482f55943617ef1fb7e9632b
Faqihazh <faqihazh@gmail.com>
2025-01-10 18:47:20 +0700

    update: home route

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+// Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/earthquakes', function () {
     return view('earthquakes.index');
+});
+
+Route::get('/', function () {
+    return view('density.index');
 });
 
 Route::get('/thematic-map/sulsel/density', function () {
